5773c91d6ddd200f316a3621b3c3e4c383d4f876
Aratakiyo <97922031+Aratakiyo@users.noreply.github.com>
2023-01-09 10:40:50 -0400

    1/9/2023

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,9 +10,12 @@ import { Combobox, Menu, Transition } from '@headlessui/react';
 import { CheckIcon, ChevronDownIcon } from '@heroicons/react/solid';
 import NextLink from 'next/link';
 
-interface Props {
+export interface Props {
   products: Product[];
+  title: string;
 }
+
+
 
 const Header = ({ products }: Props) => {
   const { data: session } = useSession();
@@ -49,7 +52,6 @@ const Header = ({ products }: Props) => {
             <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
               <Combobox.Input
                 className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-                displayValue={(prod) => prod?.title}
                 onChange={(event) => setQuery(event.target.value)}
               />
               <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
